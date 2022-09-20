bcf31fd2bc8bf0ecd3a8d94a9475b818b77ab5ef
delano-git <noronha.delano@gmail.com>
2022-09-20 22:43:45 +0400

    lemonadep: Bring up PixelExtended
    Signed-off-by: delano-git <noronha.delano@gmail.com>

--- aosp_lemonadep.mk
+++ aosp_lemonadep.mk
@@ -11,14 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonadep device
 $(call inherit-product, device/oneplus/lemonadep/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonadep
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_NAME := aosp_lemonadep
 PRODUCT_DEVICE := lemonadep
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2125
+CUSTOM_DEVICE := OnePlus9Pro
 
 PRODUCT_SYSTEM_NAME := OnePlus9Pro
 PRODUCT_SYSTEM_DEVICE := OnePlus9Pro
